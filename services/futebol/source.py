dd116e34091ecc16459f992285282efa9faf0f3f
Caio Carnelos <caio.emidio@gmail.com>
2022-08-11 00:27:40 +0100

    fix:  - horario

--- services/futebol/source.py
+++ services/futebol/source.py
@@ -1,6 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
+import datetime
 
+def convert_timezone(data, znOrigem, znDestino):
+    from zoneinfo import ZoneInfo
+    from datetime import datetime
+
+    d = datetime(data["year"], data["month"], data["day"], data["hour"], data["minute"], tzinfo=ZoneInfo(znOrigem))
+    return d.astimezone(ZoneInfo(znDestino)).strftime("%d/%m/%Y %H:%M")
 
 def montaImg(time):
     return f"https://a.espncdn.com/combiner/i?img=/i/teamlogos/soccer/500/{time}.png"
@@ -36,15 +43,63 @@ def trataTabelaResultado(tabela):
 
     return result
 
+def trata_mes(mes):
+    if mes == "jan":
+        return 1
+    if mes == "fev":
+        return 2
+    if mes == "mar":
+        return 3
+    if mes == "apr":
+        return 4
+    if mes == "mai":
+        return 5
+    if mes == "jun":
+        return 6
+    if mes == "jul":
+        return 7
+    if mes == "ago":
+        return 8
+    if mes == "set":
+        return 9
+    if mes == "out":
+        return 10
+    if mes == "nov":
+        return 11
+    if mes == "dec":
+        return 12
+
 
 def trataTabelaCalendario(tabela):
     dia = tabela.find_all('div', {'class': 'matchTeams'})
     teams = tabela.find_all('a', {'class': 'AnchorLink Table__Team'})
     tdTable = tabela.find_all('td', {'class': 'Table__TD'})
 
+    tratadoDia = dia[0].text.split(",")[1].strip().split(".")[0].replace(" ", "/")
+    diaCerto = tratadoDia.split("/")[0]
+    mesCerto = trata_mes(tratadoDia.split("/")[1])
+    currentDateTime = datetime.datetime.now()
+    date = currentDateTime.date()
+    anoCerto = date.strftime("%Y")
+    horario = tdTable[4].text
+    resultadoDia = dia[0].text.split(",")[1].strip().split(".")[0].replace(" ", "/")
+    resultadoTime = tdTable[4].text
+    if horario != "A definir":
+        horario = resultadoTime.split(":")
+        dataCorreta = {
+            "year": int(anoCerto),
+            "month": mesCerto,
+            "day": int(diaCerto),
+            "hour": int(horario[0]),
+            "minute": int(horario[1]),
+        }
+        resultado = convert_timezone(dataCorreta, 'America/New_York', "Europe/Dublin").split(" ")
+        resultadoDia = resultado[0]
+        resultadoTime = resultado[1]
+
     result = {
-        "data": dia[0].text.split(",")[1].strip().split(".")[0].replace(" ", "/"),
-        "time": tdTable[4].text,
+        "data": resultadoDia,
+        "time": resultadoTime,
         "home": trataTeam(teams, [], 0),
         "away": trataTeam(teams, [], 1),
         "championship": tdTable[5].text,
